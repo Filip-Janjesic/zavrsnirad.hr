8ef789a0e3f4de0e1e04ad6d5816f21f20d11818
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-03-02 22:09:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+// ova datoteka će definirati temeljne preduvjete
+// i napraviti autoloading
+
+session_start();
+
 define('BP',__DIR__ . DIRECTORY_SEPARATOR);
 define('BP_APP', BP . 'app' . DIRECTORY_SEPARATOR);
 
@@ -7,17 +12,21 @@ $zaAutoload=[
     BP_APP . 'controller',
     BP_APP . 'core',
     BP_APP . 'model'
-];
+]; // view ne ide u autoload jer su u njemu phtml datoteke
+
 
 $putanje = implode(PATH_SEPARATOR,$zaAutoload);
 
 set_include_path($putanje);
 
 spl_autoload_register(function($klasa){
-
+    //echo 'u spl_autoload, tražim klasu ' . $klasa . '<br>';
     $putanje = explode(PATH_SEPARATOR,get_include_path());
     foreach($putanje as $putanja){
-        $datoteka = $putanja . DIRECTORY_SEPARATOR . $klasa . '.php';
+        //echo $putanja . '<br>';
+        $datoteka = $putanja . DIRECTORY_SEPARATOR .
+                        $klasa . '.php';
+        //echo $datoteka, '<br>';
         if(file_exists($datoteka)){
             require_once $datoteka;
             break;
@@ -26,3 +35,6 @@ spl_autoload_register(function($klasa){
 });
 
 App::start();
+
+//$o = new Osoba();
+//echo $o->getIme();
