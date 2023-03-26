5e4335b425a3ec69bc81538a69b241ea1abd6b6c
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-03-26 19:58:07 +0200

    brisanje

--- app/core/App.php
+++ app/core/App.php
@@ -2,81 +2,45 @@
 
 class App
 {
-    // Ova metoda ima zadatak saznati što želiš i to pokrenuti
+
     public static function start()
     {
-        $ruta = Request::getRuta();
-        $djelovi = explode('/',substr($ruta,1));
-        $controller='';
-        if(!isset($djelovi[0]) || $djelovi[0]===''){
-            $controller='IndexController';
+
+        $Route = explode('/', Request::getRoute());
+        //CLASS
+
+        if(empty($Route[1])){
+            $class = 'Index';
         }else{
-            $controller = ucfirst($djelovi[0]) . 'Controller';
+            $class = ucfirst($Route[1]);
         }
-        $metoda='';
-        if(!isset($djelovi[1]) || $djelovi[1]==='' ){
-            $metoda='index';
+        $class .= 'Controller';
+
+        //METHOD
+
+        if(empty($Route[2])){
+            $method = 'index';
         }else{
-            $metoda=$djelovi[1];
+            $method = $Route[2];
         }
+        $Route = array_slice($Route, 3);
 
-        $parametar='';
-        if(!isset($djelovi[2]) || $djelovi[2]==='' ){
-            $parametar='';
+        //INSTANCE
+
+        if(class_exists($class) && method_exists($class, $method)){
+            $instance = New $class();
+            $instance -> $method($Route);
         }else{
-            $parametar=$djelovi[2];
+            $instance = New IndexController;
+            $instance -> error([$class,$method]);
         }
-
-        if(!(class_exists($controller) && method_exists($controller,$metoda))){
-            echo 'Ne postoji ' . $controller . '-&gt;' . $metoda;
-            return;
-        }
-        $instanca = new $controller();
-        if(strlen($parametar)>0){
-            $instanca->$metoda($parametar);
-        }else{
-            $instanca->$metoda();
-        }
-        
-    }
-
-    public static function config($kljuc)
-    {
-        $configFile = BP_APP . 'konfiguracija.php';
-
-        if(!file_exists($configFile)){
-            return 'Konfiguracijka datoteka ne postoji';
-        }
-
-        $config = require $configFile;
-
-        if(!isset($config[$kljuc])){
-            return 'Ključ ' . $kljuc . ' nije postavljen u konfiguraciji';
-        }
-
-        return $config[$kljuc];
 
     }
 
-    public static function auth()
+    public static function config(string $key) 
     {
-        return isset($_SESSION['auth']);
-    }
-
-    public static function operater()
-    {
-        return $_SESSION['auth']->ime 
-        . ' ' . $_SESSION['auth']->prezime ;
-    }
-
-    public static function admin()
-    {
-        return $_SESSION['auth']->uloga==='admin' ;
-    }
-
-    public static function dev()
-    {
-        return App::config('dev') ;
+        $config = include APP_PATH . 'config.php';
+        return $config[$key];
     }
 
 }
